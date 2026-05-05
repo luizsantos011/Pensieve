b7b997b9cc2b15c8d4196091ae2d6b649b4a83ff
luiz <luizsantost4@gmail.com>
2026-05-05 16:58:44 -0300

    Refactor: ajustes na entrada de dados

--- src/main/java/me/luiz/penseira/Main.java
+++ src/main/java/me/luiz/penseira/Main.java
@@ -1,6 +1,6 @@
 package me.luiz.penseira;
 
-import me.luiz.penseira.service.TelegramService;
+import me.luiz.penseira.bot.TelegramBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         try{
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(new TelegramService());
+            botsApi.registerBot(new TelegramBot());
             System.out.println("As águas da penseira estão calmas… prontas para receber novas memórias.");
         }catch (TelegramApiException e){
             e.printStackTrace();
--- src/main/java/me/luiz/penseira/bot/TelegramBot.java
+++ src/main/java/me/luiz/penseira/bot/TelegramBot.java
@@ -1,9 +1,12 @@
 package me.luiz.penseira.bot;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class TelegramBot extends TelegramLongPollingBot {
+    private final Dotenv dotenv = Dotenv.load();
+    private final long MEU_ID = Long.parseLong(dotenv.get("BOT_USER_ID"));
 
     @Override
     public String getBotUsername() {
@@ -12,14 +15,25 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public String getBotToken() {
-        return "8299391738:AAE7lS02qQCqXMXZBJ2NWRzlm_uMCWhG70E";
+        return dotenv.get("BOT_TOKEN");
     }
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            String texto = update.getMessage().getText();
-            System.out.println("Mensagem recebida: " + texto);
+        if (update.hasMessage()) {
+            var message = update.getMessage();
+            long userId = message.getFrom().getId();
+            if(userId != MEU_ID){
+                System.out.println("saia daqui caba safado!!");
+                return;
+            }
+            System.out.println("Mensagem recebida de " + userId);
+            if (message.hasText()) {
+                String mensagem = message.getText();
+                System.out.println();
+            }else if (message.hasDocument()) {
+                System.out.println("Recebido um documento de " + userId);
+            }
         }
     }
 }
