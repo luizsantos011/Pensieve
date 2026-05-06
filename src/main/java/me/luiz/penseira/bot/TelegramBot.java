a6bd710b951aed80cf24c801109de8925b08e80f
luiz <luizsantost4@gmail.com>
2026-05-06 00:42:59 -0300

    feat: implementar resposta automática aos comandos do telegram

--- src/main/java/me/luiz/penseira/bot/TelegramBot.java
+++ src/main/java/me/luiz/penseira/bot/TelegramBot.java
@@ -2,14 +2,16 @@
 
     import io.github.cdimascio.dotenv.Dotenv;
     import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+    import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
     import org.telegram.telegrambots.meta.api.objects.Update;
+    import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
     import java.util.*;
     import java.util.stream.Collectors;
 
     public class TelegramBot extends TelegramLongPollingBot {
         private final Dotenv dotenv = Dotenv.load();
-        private final long MEU_ID = Long.parseLong(dotenv.get("BOT_USER_ID"));
+        private final List<Long> whitelist = carregarWhitelist();
         private final Map<Long, Long> ultimaMensagemPorUsuario = new HashMap<>();
         private static final long intervaloMinimo = 5000;
 
@@ -28,12 +30,15 @@
             if (update.hasMessage()) {
                 var message = update.getMessage();
                 long userId = message.getFrom().getId();
+                SendMessage msg = new SendMessage();
+                //verificação de usuários autorizados
                 List<Long> whitelist = carregarWhitelist();
                 if(!whitelist.contains(userId)){
                     System.out.println("Apenas bruxos autorizados podem acessar esta penseira.");
                     System.out.println(userId);
                     return;
                 }
+                //verificação de frequência de mensagens
                 long agora = System.currentTimeMillis();
                 if(ultimaMensagemPorUsuario.containsKey(userId)){
                     long tempoUltimaMensagem = ultimaMensagemPorUsuario.get(userId);
@@ -46,9 +51,30 @@
                 ultimaMensagemPorUsuario.put(userId, agora);
                 System.out.println("Bruxo " + userId + " acessou a penseira.");
                 System.out.println("Mensagem recebida de " + userId);
+                //verificação do tipo da mensagem
                 if (message.hasText()) {
                     String mensagem = message.getText();
-                    System.out.println();
+                    if(mensagem.startsWith("/")) {
+                        msg.setChatId(message.getChatId().toString());
+                        msg.setText("PENSEIRA\n" +
+                                "\n" +
+                                "Sistema de Armazenamento de Memórias\n" +
+                                "\n" +
+                                "Prezado(a) usuário(a),\n" +
+                                "\n" +
+                                "Seu acesso à Penseira foi registrado.\n" +
+                                "\n" +
+                                "Este ambiente permite o armazenamento e a organização de memórias e registros, que permanecerão disponíveis para consulta sempre que necessário.\n" +
+                                "\n" +
+                                "Recomenda-se atenção ao conteúdo que decide preservar.\n" +
+                                "\n" +
+                                "A Penseira encontra-se ativa.\n");
+                        try {
+                            execute(msg);
+                        } catch (TelegramApiException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
                 }else if (message.hasDocument()) {
                     var doc = message.getDocument();
                     String nomeArquivo = doc.getFileName().toLowerCase();
