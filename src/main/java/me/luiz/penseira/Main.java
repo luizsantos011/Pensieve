5b92806029bd975a11ebcc0186af52fb5c7e978d
luiz <luizsantost4@gmail.com>
2026-05-12 23:52:15 -0300

    refactor: refatorando instanciação

--- src/main/java/me/luiz/penseira/Main.java
+++ src/main/java/me/luiz/penseira/Main.java
@@ -1,9 +1,13 @@
 package me.luiz.penseira;
 
 import me.luiz.penseira.bot.TelegramBot;
+import me.luiz.penseira.contracts.ILogger;
+import me.luiz.penseira.service.LogService;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) {
